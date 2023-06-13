0b5fd2e05bc4e1c0e20d19be727a372c2b1108c4
LaninaAstrid <134910002+LaninaAstrid@users.noreply.github.com>
2023-06-13 21:45:04 +0700

    db.js

--- authUrmood/db.js
+++ authUrmood/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'username',
-  password: 'password',
-  database: 'nama_database'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {

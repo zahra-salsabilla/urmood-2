5a782c558f6368700588d8193154ef478ac09f41
LaninaAstrid <134910002+LaninaAstrid@users.noreply.github.com>
2023-06-13 21:22:35 +0700

    connection.js

---
+++ connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting: ', err);
+    return;
+  }
+  console.log('Connect to database');
+});
+
+module.exports = db;
